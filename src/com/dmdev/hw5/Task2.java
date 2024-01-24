708f0ac0ee2dcdced899de6e6f6c18a25c570692
entgorbunov <entgorbunov@gmail.com>
2024-01-24 23:11:34 +0500

    Новая ветка

---
+++ src/com/dmdev/hw5/Task2.java
@@ -0,0 +1,30 @@
+package com.dmdev.hw5;
+/**
+ * Написать программу, удаляющую все повторяющиеся целые числа из массива и печатающую результат. Массив должен использоваться тот же самый. На место удаленных элементов ставить цифру 0.
+ */
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 4, 5}; // Исходный массив
+        removeTheSameNumber(array);
+    }
+
+    private static void removeTheSameNumber(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    array[j] = 0;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+
+}
