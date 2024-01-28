c4dfa4ef515fa325b4f5c7a0663d55b669e1bddd
Alexander Gorbunov <entgorbunov@gmail.com>
2024-01-28 13:10:31 +0500

    "Правки"

--- src/com/dmdev/functions/Task3.java
+++ src/com/dmdev/functions/Task3.java
@@ -1,31 +1,20 @@
 package com.dmdev.functions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * Даны два прямоугольных треугольника.
- * Каждый из них задается двумя целочисленными переменными a и b - стороны треугольника.
- * Написать код, в котором вычисляется площадь каждого треугольника и затем эти площади сравниваются друг с другом.
- *
- * Для этого понадобится написать 2 функции.
- * Первая: по двум сторонам прямоугольного треугольника возвращает его площадь.
- * Вторая: сравнивает переданные площади двух треугольников и выводит на консоль первый треугольник больше, меньше или равен второму.
- * Учитывать, что площадь может быть вещественным числом.
- */
 public class Task3 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input value for first side of first triangular: ");
-        var value1 = scanner.nextDouble();
-        System.out.println("Input value for second side of first triangular: ");
-        var value2 = scanner.nextDouble();
-        System.out.println("Input value for first side of second triangular: ");
-        var value3 = scanner.nextDouble();
-        System.out.println("Input value for second side of second triangular: ");
-        var value4 = scanner.nextDouble();
 
-        double area1 = calculateArea(value1, value2);
-        double area2 = calculateArea(value3, value4);
+        double side1Triangle1 = getDoubleInput(scanner, "Введите значение для первой стороны первого треугольника: ");
+        double side2Triangle1 = getDoubleInput(scanner, "Введите значение для второй стороны первого треугольника: ");
+        double side1Triangle2 = getDoubleInput(scanner, "Введите значение для первой стороны второго треугольника: ");
+        double side2Triangle2 = getDoubleInput(scanner, "Введите значение для второй стороны второго треугольника: ");
+
+        double area1 = calculateArea(side1Triangle1, side2Triangle1);
+        double area2 = calculateArea(side1Triangle2, side2Triangle2);
 
         compareAreas(area1, area2);
     }
@@ -36,11 +25,23 @@ public class Task3 {
 
     public static void compareAreas(double area1, double area2) {
         if (area1 > area2) {
-            System.out.println("Первый треугольник больше второго.");
+            System.out.println("Площадь первого треугольника больше.");
         } else if (area1 < area2) {
-            System.out.println("Первый треугольник меньше второго.");
+            System.out.println("Площадь второго треугольника больше.");
         } else {
-            System.out.println("Треугольники равны.");
+            System.out.println("Площади треугольников равны.");
+        }
+    }
+
+    private static double getDoubleInput(Scanner scanner, String message) {
+        while (true) {
+            System.out.println(message);
+            try {
+                return scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Некорректный ввод, пожалуйста, введите числовое значение.");
+                scanner.next();
+            }
         }
     }
 }
