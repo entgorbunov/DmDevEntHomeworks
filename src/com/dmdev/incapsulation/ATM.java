431959dde7aa8e4f426fba09bc35a04226703bdd
Alexander Gorbunov <entgorbunov@gmail.com>
2024-02-18 14:22:56 +0500

    финальные правки

--- src/com/dmdev/incapsulation/ATM.java
+++ src/com/dmdev/incapsulation/ATM.java
@@ -1,11 +1,14 @@
 package com.dmdev.incapsulation;
 
 
+import java.util.Scanner;
 
 public class ATM {
     private int quantity10;
     private int quantity20;
     private int quantity50;
+
+    Scanner scanner = new Scanner(System.in);
 
     public ATM(int quantity50, int quantity20, int quantity10) {
         this.quantity50 = quantity50;
@@ -13,14 +16,14 @@ public class ATM {
         this.quantity10 = quantity10;
     }
 
-    protected void addMoney(int addQuantity50, int addQuantity20, int addQuantity10) {
+    public void addMoney(int addQuantity50, int addQuantity20, int addQuantity10) {
         this.quantity50 += addQuantity50;
         this.quantity20 += addQuantity20;
         this.quantity10 += addQuantity10;
         System.out.println("Добавлено в банкомат: 50x" + addQuantity50 + ", 20x" + addQuantity20 + ", 10x" + addQuantity10);
     }
 
-    protected boolean withdrawMoney(int amount) {
+    public boolean withdrawMoney(int amount) {
         if (isInvalidAmount(amount)) {
             return false;
         }
@@ -42,44 +45,48 @@ public class ATM {
         }
     }
 
-    protected boolean isInvalidAmount(int amount) {
-        if (amount <= 0 || amount > sumOfMoney()) {
-            System.out.println("Некорректная сумма или недостаточно средств.");
-            return true;
-        }
-        return false;
+    public boolean isInvalidAmount(int amount) {
+        return amount <= 0 || amount > sumOfMoney();
     }
 
-    protected int calculateRequiredBillsAtStart(int amount, int billValue, int quantity) {
+    public int calculateRequiredBillsAtStart(int amount, int billValue, int quantity) {
         return Math.min(amount / billValue, quantity);
     }
 
-    protected int calculateRequiredBills(int remainingAmount, int billValue, int quantity) {
+    public int calculateRequiredBills(int remainingAmount, int billValue, int quantity) {
         return Math.min(remainingAmount / billValue, quantity);
     }
 
-    protected boolean isRemainingAmountZero(int remainingAmount) {
+    public boolean isRemainingAmountZero(int remainingAmount) {
         return remainingAmount == 0;
     }
 
-    protected void updateQuantities(int required50, int required20, int required10) {
+    public void updateQuantities(int required50, int required20, int required10) {
         this.quantity50 -= required50;
         this.quantity20 -= required20;
         this.quantity10 -= required10;
     }
 
-    protected void printWithdrawalDetails(int required50, int required20, int required10) {
+    public void printWithdrawalDetails(int required50, int required20, int required10) {
         System.out.println("Снято денег: 50x" + required50 + ", 20x" + required20 + ", 10x" + required10);
         System.out.println("Осталось средств: " + sumOfMoney());
     }
 
-    protected void printSumOfMoney() {
+    public void printSumOfMoney() {
         System.out.println("Средств на счету: " + sumOfMoney());
     }
 
-    protected int sumOfMoney() {
+    public int sumOfMoney() {
         return quantity50 * 50 + quantity20 * 20 + quantity10 * 10;
     }
+    public void isWithdrawMoney(ATM atm) {
+        System.out.println("Введите сумму для снятия: ");
+        int amount = scanner.nextInt();
+        if(atm.withdrawMoney(amount)) System.out.println("Деньги успешно сняты.");
+        else System.out.println("Некорректная сумма или недостаточно средств.");
+    }
+
+
 
 }
 
--- src/com/dmdev/incapsulation/ATMRunner.java
+++ src/com/dmdev/incapsulation/ATMRunner.java
@@ -5,7 +5,9 @@ public class ATMRunner {
         ATM atm = new ATM(0, 0, 0);
         atm.addMoney(2, 3, 5);
         atm.printSumOfMoney();
-        boolean result = atm.withdrawMoney(220);
-        System.out.println("Выдача прошла успешно: " + result);
+        atm.isWithdrawMoney(atm);
+
     }
+
+
 }
